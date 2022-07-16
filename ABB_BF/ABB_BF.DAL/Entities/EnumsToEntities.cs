c758d175728b20ef7ac34078d8536f314457c068
Emil <intestinal_d@rambler.ru>
2022-07-17 02:19:49 +0300

    Added EnumsToEntities entity

---
+++ ABB_BF/ABB_BF.DAL/Entities/EnumsToEntities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABB_BF.DAL.Entities
+{
+    public class EnumsToEntities
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public string Definition { get; set; }
+    }
+}
