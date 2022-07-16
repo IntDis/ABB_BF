a9df7fe68fd47e2fdcf512491f6c1b04426c8242
Roman Azarov <azarovrom9215@gmail.com>
2022-07-16 19:10:18 +0300

    added college repositry

---
+++ ABB_BF/ABB_BF.DAL/Repositories/Interfaces/ICollegeRepository.cs
@@ -0,0 +1,10 @@
+﻿using ABB_BF.DAL.Entities;
+
+namespace ABB_BF.DAL.Repositories.Interfaces
+{
+    public interface ICollegeRepository
+    {
+        Task AddCollege(College college);
+        Task<List<College>> GetAllColleges();
+    }
+}
