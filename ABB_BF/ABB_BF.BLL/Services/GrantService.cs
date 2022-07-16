a3294c6158d61e15c34d7e5dff42ba1c94c31c3c
Emil <intestinal_d@rambler.ru>
2022-07-17 01:20:53 +0300

    Implemented ModelsService

--- ABB_BF/ABB_BF.BLL/Services/GrantService.cs
+++ ABB_BF/ABB_BF.BLL/Services/GrantService.cs
@@ -12,16 +12,33 @@ namespace ABB_BF.BLL.Services
         private readonly IMapper _mapper;
         private readonly IGrantRepository _grantRepository;
         private readonly IFileHelper _fileHelper;
+        private readonly IModelsService _modelsService;
 
-        public GrantService(IMapper mapper, IGrantRepository grantRepository, IFileHelper fileHelper)
+        public GrantService(
+            IMapper mapper, 
+            IGrantRepository grantRepository, 
+            IFileHelper fileHelper, 
+            IModelsService modelsService)
         {
             _mapper = mapper;
             _grantRepository = grantRepository;
             _fileHelper = fileHelper;
+            _modelsService = modelsService;
         }
 
         public async Task<int> AddGrant(GrantModel grantModel)
         {
+            if (!_modelsService.IsNumberValid(grantModel.Phone))
+            {
+                throw new ArgumentException("Введен неверный номер");
+            }
+            else if (!_modelsService.IsEmailValid(grantModel.Email))
+            {
+                throw new ArgumentException("Введен неверный e-mail");
+            }
+
+            grantModel.Phone = _modelsService.FixPhoneNumber(grantModel.Phone);
+
             grantModel.CreationDate = DateOnly.FromDateTime(DateTime.Now);
 
             Grant grant = _mapper.Map<Grant>(grantModel);
