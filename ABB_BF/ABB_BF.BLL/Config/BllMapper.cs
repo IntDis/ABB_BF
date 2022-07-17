85ed507904ee9b4f22fdf2e4eb35534d5f8fa158
Emil <intestinal_d@rambler.ru>
2022-07-17 18:09:43 +0300

    Added processing for request models fields
    Full names, emails, phones, colleges

--- ABB_BF/ABB_BF.BLL/Config/BllMapper.cs
+++ ABB_BF/ABB_BF.BLL/Config/BllMapper.cs
@@ -2,6 +2,7 @@
 using ABB_BF.DAL.Entities;
 using ABB_BF.DAL.Models;
 using AutoMapper;
+using System.Text;
 
 namespace ABB_BF.BLL.Config
 {
@@ -9,10 +10,34 @@ namespace ABB_BF.BLL.Config
     {
         public BllMapper()
         {
-            CreateMap<Probation, ProbationModel>().ReverseMap();
-            CreateMap<Grant, GrantModel>().ReverseMap();
-            CreateMap<University, UniversityModel>().ReverseMap();
-            CreateMap<Practice, PracticeModel>().ReverseMap();
+            CreateMap<Probation, ProbationModel>().ReverseMap()
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email.Trim().ToLower()))
+                .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => FixPhoneNumber(src.Phone.Trim().ToLower())))
+                .ForMember(dest => dest.Firstname, opt => opt.MapFrom(src => FirstUpper(src.Firstname.Trim())))
+                .ForMember(dest => dest.Secondname, opt => opt.MapFrom(src => FirstUpper(src.Secondname.Trim())))
+                .ForMember(dest => dest.Patronymic, opt => opt.MapFrom(src => FirstUpper(src.Patronymic.Trim())));
+
+            CreateMap<Grant, GrantModel>().ReverseMap()
+                .ForMember(dest => dest.College, opt => opt.MapFrom(src => src.College.Trim().ToLower()))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email.Trim().ToLower()))
+                .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => FixPhoneNumber(src.Phone.Trim().ToLower())))
+                .ForMember(dest => dest.Firstname, opt => opt.MapFrom(src => FirstUpper(src.Firstname.Trim())))
+                .ForMember(dest => dest.Secondname, opt => opt.MapFrom(src => FirstUpper(src.Secondname.Trim())))
+                .ForMember(dest => dest.Patronymic, opt => opt.MapFrom(src => FirstUpper(src.Patronymic.Trim())));
+
+            CreateMap<University, UniversityModel>().ReverseMap()
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email.Trim().ToLower()))
+                .ForMember(dest => dest.Firstname, opt => opt.MapFrom(src => FirstUpper(src.Firstname.Trim())))
+                .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => FixPhoneNumber(src.Phone.Trim().ToLower())))
+                .ForMember(dest => dest.Secondname, opt => opt.MapFrom(src => FirstUpper(src.Secondname.Trim())))
+                .ForMember(dest => dest.Patronymic, opt => opt.MapFrom(src => FirstUpper(src.Patronymic.Trim())));
+
+            CreateMap<Practice, PracticeModel>().ReverseMap()
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email.Trim().ToLower()))
+                .ForMember(dest => dest.Firstname, opt => opt.MapFrom(src => FirstUpper(src.Firstname.Trim())))
+                .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => FixPhoneNumber(src.Phone.Trim().ToLower())))
+                .ForMember(dest => dest.Secondname, opt => opt.MapFrom(src => FirstUpper(src.Secondname.Trim())))
+                .ForMember(dest => dest.Patronymic, opt => opt.MapFrom(src => FirstUpper(src.Patronymic.Trim())));
 
             CreateMap<ProbationFileModel, ProbationFile>().ReverseMap();
             CreateMap<AbstractFormFileModel, ProbationFile>().ReverseMap();
@@ -24,7 +49,31 @@ namespace ABB_BF.BLL.Config
             CreateMap<AbstractEntityModel, AbstractCommonData>().ReverseMap();
 
             CreateMap<FilterModel, Filter>().ReverseMap();
-            CreateMap<CollegeModel, College>().ReverseMap();
+            CreateMap<College, CollegeModel>().ReverseMap()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name.Trim().ToLower()));
+        }
+        private string FirstUpper(string str)
+        {
+            return str.Substring(0, 1).ToUpper() + (str.Length > 1 ? str.Substring(1) : "").ToLower();
+        }
+
+        private string FixPhoneNumber(string number)
+        {
+            StringBuilder sb = new(number);
+
+            if (sb.ToString().StartsWith("+7"))
+            {
+                sb.Replace("+7", "8");
+            }
+
+            foreach (char symbol in number)
+            {
+                sb.Replace("(", null);
+                sb.Replace("-", null);
+                sb.Replace(")", null);
+            }
+
+            return sb.ToString();
         }
     }
 }
