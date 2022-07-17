ca4046fb325c3e1aea2ab6fb006b6b6ec03bcf8b
Roman Azarov <azarovrom9215@gmail.com>
2022-07-17 18:30:43 +0300

    added speciality and educationlvl inot onModelCreating

--- ABB_BF/ABB_BF.DAL/Context.cs
+++ ABB_BF/ABB_BF.DAL/Context.cs
@@ -74,6 +74,96 @@ namespace ABB_BF.DAL
                 }
             );
 
+            modelBuilder.Entity<EducationForm>().HasData(
+                new EducationForm()
+                {
+                    Id = 1,
+                    Number = 1,
+                    Definition = "Колледж/Техникум"
+                },
+                new EducationForm()
+                {
+                    Id = 2,
+                    Number = 2,
+                    Definition = "Бакалавриат"
+                },
+                new EducationForm()
+                {
+                    Id = 3,
+                    Number = 3,
+                    Definition = "Специалитет"
+                },
+                new EducationForm()
+                {
+                    Id = 4,
+                    Number = 4,
+                    Definition = "Магистратура"
+                }
+            );
+
+            modelBuilder.Entity<Speciality>().HasData(
+                new EducationForm()
+                {
+                    Id = 1,
+                    Number = 1,
+                    Definition = "экономика"
+                },
+                new EducationForm()
+                {
+                    Id = 2,
+                    Number = 2,
+                    Definition = "банковское дело"
+                },
+                new EducationForm()
+                {
+                    Id = 3,
+                    Number = 3,
+                    Definition = "кредиты и финансы"
+                },
+                new EducationForm()
+                {
+                    Id = 4,
+                    Number = 4,
+                    Definition = "рынок ценных бумаг"
+                },
+                new EducationForm()
+                {
+                    Id = 5,
+                    Number = 5,
+                    Definition = "информационные технологии"
+                },
+                new EducationForm()
+                {
+                    Id = 6,
+                    Number = 6,
+                    Definition = "бизнес-информатика"
+                },
+                new EducationForm()
+                {
+                    Id = 7,
+                    Number = 7,
+                    Definition = "бизнес-аналитика"
+                },
+                new EducationForm()
+                {
+                    Id = 8,
+                    Number = 8,
+                    Definition = "финансовый менеджмент"
+                },
+                new EducationForm()
+                {
+                    Id = 9,
+                    Number = 9,
+                    Definition = "информационная безопасность"
+                },
+                new EducationForm()
+                {
+                    Id = 10,
+                    Number = 10,
+                    Definition = "математическое моделирование (прикладная математика)"
+                }
+            );
+
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
