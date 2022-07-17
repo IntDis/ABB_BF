7a572e1d88f9718041f9cfd5f52a9a80cf87f609
Emil <intestinal_d@rambler.ru>
2022-07-17 15:52:35 +0300

    Added migration "UpdatedGrantWithEducationFormField"

--- ABB_BF/ABB_BF.DAL/Migrations/20220717124537_UpdatedGrantWithEducationFormField.Designer.cs
+++ ABB_BF/ABB_BF.DAL/Migrations/20220717124537_UpdatedGrantWithEducationFormField.Designer.cs
@@ -4,6 +4,7 @@ using ABB_BF.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ABB_BF.DAL.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20220717124537_UpdatedGrantWithEducationFormField")]
+    partial class UpdatedGrantWithEducationFormField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
