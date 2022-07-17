2a61094eef3e3e048aabe7a11434e0f722eef001
Emil <intestinal_d@rambler.ru>
2022-07-17 03:25:58 +0300

    Added EnumsToEntitiesRepository
    with intefrace

---
+++ ABB_BF/ABB_BF.DAL/Repositories/EnumsToEntitiesRepository.cs
@@ -0,0 +1,35 @@
+﻿using ABB_BF.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABB_BF.DAL.Repositories
+{
+    public class EnumsToEntitiesRepository : IEnumsToEntitiesRepository
+    {
+        private readonly Context _context;
+
+        public async Task<string> GetDefinitionByNumberFromCourseDirections(int number)
+        {
+            return (await _context.CourseDirections.FirstOrDefaultAsync(o => o.Number == number)).Definition;
+        }
+
+        public async Task<string> GetDefinitionByNumberFromEducationForms(int number)
+        {
+            return (await _context.EducationForms.FirstOrDefaultAsync(o => o.Number == number)).Definition;
+        }
+
+        public async Task<string> GetDefinitionByNumberFromEducationLevels(int number)
+        {
+            return (await _context.EducationLevels.FirstOrDefaultAsync(o => o.Number == number)).Definition;
+        }
+        public async Task<string> GetDefinitionByNumberFromSpecialities(int number)
+        {
+            return (await _context.EducationLevels.FirstOrDefaultAsync(o => o.Number == number)).Definition;
+        }
+
+    }
+}
---
+++ ABB_BF/ABB_BF.DAL/Repositories/Interfaces/IEnumsToEntitiesRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ABB_BF.DAL.Repositories.Interfaces
+{
+    public interface IEnumsToEntitiesRepository
+    {
+        Task<string> GetDefinitionByNumberFromCourseDirections(int number);
+        Task<string> GetDefinitionByNumberFromEducationForms(int number);
+        Task<string> GetDefinitionByNumberFromEducationLevels(int number);
+        Task<string> GetDefinitionByNumberFromSpecialities(int number);
+    }
+}
